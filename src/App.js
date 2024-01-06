7569107bb743bd7046e8f81b13bd8abc7325b1e2
Chris <117781333+chrism-la@users.noreply.github.com>
2024-01-06 15:48:25 -0800

    Updated react render method as well as wrapped <route> tags in <routes> tag

--- src/App.js
+++ src/App.js
@@ -1,53 +1,45 @@
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 
-import Home from './components/Home'
-import About from './components/About'
-import Packages from './components/Packages'
-
+import Home from './components/Home';
+import About from './components/About';
+import Packages from './components/Packages';
 
 function App() {
-  
-  const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath']
+    const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath'];
 
+    return (
+        <div className="App">
+            <Router>
+                <header>
+                    <h1 className="title">Welcome to Monty's Mineral SPA</h1>
 
-  return (
-    <div className="App">
-      <Router>
-        <header>
-          <h1 className="title">Welcome to Monty's Mineral SPA</h1>
+                    <div className="navBar">
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About Us</Link>
+                            </li>
+                            <li>
+                                <Link to="/packages">Our Packages</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </header>
 
-          <div className="navBar">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About Us</Link>
-              </li>
-              <li>
-                <Link to="/packages">Our Packages</Link>
-              </li>
-            </ul>
-          </div>
-        </header>
-
-        <div className="display">
-          <Route path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/packages" render={() => <Packages packages={packages}/>}  />
+                <div className="display">
+                    <Routes>
+                        <Route path="/" component={Home} />
+                        <Route path="/about" component={About} />
+                        <Route path="/packages" render={() => <Packages packages={packages} />} />
+                    </Routes>
+                </div>
+            </Router>
         </div>
-
-      </Router>
-
-    </div>
-  );
+    );
 }
 
 export default App;
-
-
-
-
-
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-
+createRoot(document.getElementById('root')).render(<App />);
